c9aa3591fce539e8a3c57b49c69649062db1a8ff
IvanniaEdith <ivannia.arellano@cetys.edu.mx>
2019-05-03 18:48:58 -0700

    nuevas pantallas adeudos_row e historial_f_row

---
+++ MiCampusReact/Utils/historial_f_row.js
@@ -0,0 +1,134 @@
+import React from 'react';
+import {
+    Text,
+    View,
+    StyleSheet,
+    ScrollView,
+} from 'react-native';
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
+
+class HistorialFRow extends React.Component {
+
+    render() {
+        return (
+            <View style={styles.container}>
+            <ScrollView style={{marginBottom:50}}>
+
+                <View style={styles.fila}>
+                    <View style={[styles.seleccionar]}>
+                    </View>
+                    <View>
+                        <View style={[styles.precio]}>
+                            <Text style={styles.headersPrecio}>$165</Text>
+                        </View>
+                        <View style={styles.info}>
+                            <Text style={styles.texto}>Credenciales</Text>
+                            <Text style={styles.headers}>Duplicado de credencial de estudiante</Text>
+                        </View>
+                    </View>
+                </View>
+                
+                </ScrollView>
+                </View>
+                
+        );
+    }
+}
+
+export default HistorialFRow;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    fila: {
+        marginTop: 30,
+        marginLeft: 15,
+        marginBottom: 5,
+        flexDirection: 'row',
+    },
+    seleccionar: {
+        justifyContent: 'center',
+        height: 100,
+        width: wp('15%'),
+        borderTopLeftRadius: 10,
+        borderBottomLeftRadius: 10,
+        backgroundColor: '#ffffff',
+        flexDirection: 'column',
+        shadowColor: 'grey',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.9,
+        shadowRadius: 2,
+        elevation: 4,
+    },
+    precio: {
+        height: 30,
+        width: wp('76%'),
+        borderTopRightRadius: 10,
+        backgroundColor: '#666666',
+        flexDirection: 'column',
+        shadowColor: 'grey',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.9,
+        shadowRadius: 2,
+        elevation: 4,
+    },
+    info: {
+        height: 70,
+        width: wp('76%'),
+        borderBottomRightRadius: 10,
+        backgroundColor: '#f2f2f2',
+        flexDirection: 'column',
+        shadowColor: 'grey',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.9,
+        shadowRadius: 2,
+        elevation: 4,
+    },
+    total: {
+      backgroundColor: '#191919',
+      bottom: 0,
+      position: 'absolute',
+      padding: 7,
+      width: '100%',
+    },
+    texto: {
+        fontSize: 12,
+        textAlign: 'left',
+        marginLeft: 18,
+        color: '#333333',
+        marginTop: 7,
+        opacity: 1,
+    },
+    headersPrecio: {
+        fontSize: 14,
+        textAlign: 'right',
+        marginRight: 15,
+        color: 'white',
+        marginTop: 7,
+      },
+    headers: {
+        fontSize: 14,
+        fontWeight: 'bold', 
+        textAlign: 'left',
+        marginLeft: 18,
+        marginRight: 15,
+        marginTop: 4,
+        color: '#333333',
+      },
+      caja: {
+        width: 20,
+        height: 20,
+
+      },
+    cuadrado: {
+        position: 'absolute',
+        backgroundColor: '#e6e6e6',
+        marginLeft: 20,
+    },
+    palomita: {
+        height: 100,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+});
--- MiCampusReact/Ventanas/Adeudos.js
+++ MiCampusReact/Ventanas/Adeudos.js
@@ -1,168 +1,58 @@
 import React from 'react';
 import {
-    Text,
     View,
     StyleSheet,
-    ScrollView,
-    TouchableOpacity,
-    Image
+    FlatList
 } from 'react-native';
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
+import AdeudosRow from '../Utils/adeudos_row';
 
-export default class Adeudos extends React.Component {
+class Adeudos extends React.Component {
 
-    _IraPago = () => {
-        this.props.navigation.navigate('Pago');
-      }
+    
+      _renderItem = ({ item }) => (
+        <AdeudosRow
+        onPressItem={this._onPressItem}
+        fila={item.fila}
+        seleccionar={item.seleccionar}
+        headersPrecio={item.headersPrecio}
+        info={item.info}
+        texto={item.texto}
+        header={item.header}
+        />
+      );
 
-    render() {
+      render() {
         return (
-            <View style={styles.container}>
-
-            <ScrollView style={{marginBottom:50}}>
-
-                <View style={styles.fila}>
-                    <View style={[styles.seleccionar]}>
-                    </View>
-                    <View>
-                        <View style={[styles.precio]}>
-                            <Text style={styles.headersPrecio}>$165</Text>
-                        </View>
-                        <View style={styles.info}>
-                            <Text style={styles.texto}>Credenciales</Text>
-                            <Text style={styles.headers}>Duplicado de credencial de estudiante</Text>
-                        </View>
-                    </View>
-                </View>
-                <View style={styles.fila}>
-                    <View style={[styles.seleccionar]}>
-                    </View>
-                    <View>
-                        <View style={[styles.precio]}>
-                            <Text style={styles.headersPrecio}>$65</Text>
-                        </View>
-                        <View style={styles.info}>
-                            <Text style={styles.texto}>Escolares (español)</Text>
-                            <Text style={styles.headers}>Carta para servicios médicos</Text>
-                        </View>
-                    </View>
-                </View>
-                <View style={styles.fila}>
-                    <View style={[styles.seleccionar]}>
-                    </View>
-                    <View>
-                        <View style={[styles.precio]}>
-                            <Text style={styles.headersPrecio}>$65</Text>
-                        </View>
-                        <View style={styles.info}>
-                            <Text style={styles.texto}>Escolares (español)</Text>
-                            <Text style={styles.headers}>Constancia de estudios (sin calificaciones)</Text>
-                        </View>
-                    </View>
-                </View>
-                <View style={styles.fila}>
-                    <View style={[styles.seleccionar]}>
-                    </View>
-                    <View>
-                        <View style={[styles.precio]}>
-                            <Text style={styles.headersPrecio}>$65</Text>
-                        </View>
-                        <View style={styles.info}>
-                            <Text style={styles.texto}>Escolares (español)</Text>
-                            <Text style={styles.headers}>Carta de calificaciones</Text>
-                        </View>
-                    </View>
-                </View>
-                <View style={styles.fila}>
-                    <View style={[styles.seleccionar]}>
-                    </View>
-                    <View>
-                        <View style={[styles.precio]}>
-                            <Text style={styles.headersPrecio}>$870</Text>
-                        </View>
-                        <View style={styles.info}>
-                            <Text style={styles.texto}>Escolares (español)</Text>
-                            <Text style={styles.headers}>Trámite de cambio de carrera y/o maestría (convalidación de estudios)</Text>
-                        </View>
-                    </View>
-                </View>
-
-                </ScrollView>
-                </View>     
+          <View style={styles.container} >
+            <FlatList
+              data={this.state.data}
+              extraData={this.state}
+              keyExtractor={(item, index) => item.fila}
+              renderItem={this._renderItem}
+              showsVerticalScrollIndicator={false}
+            />
+            </View>
+                
         );
     }
 }
+export default Adeudos;
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
     },
-    fila: {
-        marginTop: 30,
-        marginLeft: 15,
-        marginBottom: 5,
-        flexDirection: 'row',
+    textPromedio: { 
+      color: 'white', 
+      fontWeight: 'bold', 
+      fontSize: 18, 
+      textAlign: 'center', 
+      marginTop: 3,
+      marginBottom: 20, 
     },
-    seleccionar: {
-        justifyContent: 'center',
-        height: 100,
-        width: wp('15%'),
-        borderTopLeftRadius: 10,
-        borderBottomLeftRadius: 10,
-        backgroundColor: '#ffffff',
-        flexDirection: 'column',
-        shadowColor: 'grey',
-        shadowOffset: { width: 0, height: 2 },
-        shadowOpacity: 0.9,
-        shadowRadius: 2,
-        elevation: 4,
-    },
-    precio: {
-        height: 30,
-        width: wp('76%'),
-        borderTopRightRadius: 10,
-        backgroundColor: '#666666',
-        flexDirection: 'column',
-        shadowColor: 'grey',
-        shadowOffset: { width: 0, height: 2 },
-        shadowOpacity: 0.9,
-        shadowRadius: 2,
-        elevation: 4,
-    },
-    info: {
-        height: 70,
-        width: wp('76%'),
-        borderBottomRightRadius: 10,
-        backgroundColor: '#f2f2f2',
-        flexDirection: 'column',
-        shadowColor: 'grey',
-        shadowOffset: { width: 0, height: 2 },
-        shadowOpacity: 0.9,
-        shadowRadius: 2,
-        elevation: 4,
-    },
-    texto: {
-        fontSize: 12,
-        textAlign: 'left',
-        marginLeft: 18,
-        color: '#333333',
-        marginTop: 7,
-        opacity: 1,
-    },
-    headersPrecio: {
-        fontSize: 14,
-        textAlign: 'right',
-        marginRight: 15,
-        color: 'white',
-        marginTop: 7,
-      },
-    headers: {
-        fontSize: 14,
-        fontWeight: 'bold', 
-        textAlign: 'left',
-        marginLeft: 18,
-        marginRight: 15,
-        marginTop: 4,
-        color: '#333333',
+    boton: { 
+      marginLeft: 25,
+      height: 10,
       },
 });
