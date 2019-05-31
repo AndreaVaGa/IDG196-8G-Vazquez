df7a20fef8293914b5f7f148ec1e8af51693ce4d
IvanniaEdith <ivannia.arellano@cetys.edu.mx>
2019-05-31 14:36:59 -0700

    margenes en historial financiero arreglados

--- MiCampusReact/Utils/historial_f_row.js
+++ MiCampusReact/Utils/historial_f_row.js
@@ -30,6 +30,8 @@ class HistorialFRow extends React.Component {
                         <View style={styles.info}>
                             <Text style={styles.headers}>Compra #{this.props.id_compra}</Text>
                             <Text style={styles.texto}>{this.props.descripcion}...</Text>
+                        </View>
+                        <View style={styles.reciboTabla}>
                             <TouchableOpacity onPress={this._IraRecibo}>
                                 <Text style={[styles.reciboText]}>Ver recibo</Text>
                             </TouchableOpacity>
@@ -69,7 +71,7 @@ const styles = StyleSheet.create({
         elevation: 4,
     },
     precio: {
-        height: 30,
+        height: 20,
         width: wp('66%'),
         borderTopRightRadius: 10,
         backgroundColor: '#666666',
@@ -81,7 +83,18 @@ const styles = StyleSheet.create({
         elevation: 4,
     },
     info: {
-        height: 80,
+        height: 70,
+        width: wp('66%'),
+        backgroundColor: '#f2f2f2',
+        flexDirection: 'column',
+        shadowColor: 'grey',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.9,
+        shadowRadius: 2,
+        elevation: 4,
+    },
+    reciboTabla: {
+        height: 20,
         width: wp('66%'),
         borderBottomRightRadius: 10,
         backgroundColor: '#f2f2f2',
@@ -96,15 +109,16 @@ const styles = StyleSheet.create({
         backgroundColor: '#191919',
         bottom: 0,
         position: 'absolute',
-        padding: 7,
+        padding: 5,
         width: '100%',
     },
     texto: {
         fontSize: 12,
         textAlign: 'left',
-        marginLeft: 10,
+        marginLeft: 15,
+        marginRight: 15,
         color: '#333333',
-        marginTop: 7,
+        marginTop: 5,
         opacity: 1,
     },
     headersPrecio: {
@@ -112,13 +126,13 @@ const styles = StyleSheet.create({
         textAlign: 'right',
         marginRight: 15,
         color: 'white',
-        marginTop: 7,
+        marginTop: 2,
     },
     headers: {
         fontSize: 14,
         fontWeight: 'bold',
         textAlign: 'left',
-        marginLeft: 18,
+        marginLeft: 15,
         marginRight: 15,
         marginTop: 4,
         color: '#333333',
@@ -127,8 +141,7 @@ const styles = StyleSheet.create({
         fontSize: 10,
         fontWeight: 'bold',
         textAlign: 'right',
-        marginRight: 18,
-        marginTop: 1,
+        marginRight: 15,
         color: 'grey',
     },
     fecha: {
@@ -139,6 +152,5 @@ const styles = StyleSheet.create({
     caja: {
         width: 20,
         height: 20,
-
     }
 });
